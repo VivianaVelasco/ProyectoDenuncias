d8f29c57c0193384579b7466e4ea3dcabe1fbd36
AnthonyTerreros <dterrero@espol.edu.ec>
2023-09-05 17:12:39 -0500

    correcciones

--- denunciasapp/lib/presentation/widgets/complaint.dart
+++ denunciasapp/lib/presentation/widgets/complaint.dart
@@ -8,47 +8,43 @@ class Complaint extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    //final size = MediaQuery.of(context).size;
+    final size = MediaQuery.of(context).size;
     // final colorTheme = Theme.of(context).colorScheme;
     return GestureDetector(
       child: Column(
         children: [
-          //Container(
-            //height: 100,
-           // width: (size.width * 0.90),
-            //decoration: BoxDecoration(
-                //color: Colors.blueGrey[50],
-                //border: Border.all(color: Colors.black),
-                //borderRadius: BorderRadius.circular(5)),
-            //child: Padding(
-              //padding:
-                  //const EdgeInsets.symmetric(horizontal: 20.0, vertical: 10.0),
-              //child: 
-              //Text(
-                //denuncia.title,
-                //style: const TextStyle(color: Colors.black),
-              //),
-              
-              Card(
-                margin: EdgeInsetsGeometry.infinity,
+          Container(
+            height: 150,
+            width: (size.width * 0.90),
+            decoration: BoxDecoration(
                 color: Colors.blueGrey[50],
-                child: ListTile(
-                  title: Text(denuncia.title, style: const TextStyle(color: Colors.black),),
-                  subtitle: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(denuncia.motivo.name),
-                      Text(denuncia.parroquia.name),
-                      Text(denuncia.description)
-                    ],
-                  ),
+                border: Border.all(color: Colors.black),
+                borderRadius: BorderRadius.circular(5)),
+            child: Padding(
+              padding:
+                  const EdgeInsets.symmetric(horizontal: 20.0, vertical: 10.0),
+              child: ListTile(
+                title: Text(
+                  denuncia.title,
+                  style: const TextStyle(
+                      color: Colors.black, fontWeight: FontWeight.bold),
+                ),
+                subtitle: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      denuncia.motivo.name,
+                    ),
+                    Text(denuncia.parroquia.name),
+                    Text(
+                      denuncia.description,
+                      style: const TextStyle(overflow: TextOverflow.ellipsis),
+                    )
+                  ],
                 ),
               ),
-
-
-
-            //),
-          //),
+            ),
+          ),
           const SizedBox(height: 10),
         ],
       ),
